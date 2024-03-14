f614bff42b3ec14298635ecf787220477ca97e1a
John Lucas Hill <johnlucas.hill@gmail.com>
2024-03-14 17:51:04 -0400

    added photos and alts to quis answers

--- app.js
+++ app.js
@@ -20,28 +20,28 @@ const questions = [
     text: "What spirit needs to be the base of this drink?",
     answers: [
         {
-            text: "Bourbon",
-            image: "",
-            alt: "Blanton's Bourbon",
-            credit: "",
+            text: "Whiskey",
+            image: "https://images.unsplash.com/photo-1574713600544-ca13b5d573d5?q=80&w=1615&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            alt: "Whiskey barrels in a rickhouse",
+            credit: "Photo by Daniel Norris on Unsplash",
         },
         {
             text: "Vodka",
-            image: "",
-            alt: "Grey Goose Vodka",
-            credit: "",
+            image: "https://images.unsplash.com/photo-1595430712287-744ad635e266?q=80&w=1508&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            alt: "A bottle of Absolut Vodka",
+            credit: "Photo by Tim Rüßmann on Unsplash",
         },
         {
             text: "Gin",
-            image: "",
-            alt: "Bombay Sapphire Gin",
-            credit: "",
+            image: "https://images.unsplash.com/photo-1597512651354-853bb81bcdad?q=80&w=1528&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            alt: "Juniper Berries",
+            credit: "Photo by Sébastien Noël on Unsplash",
         },
         {
-            text: "Tequila",
-            image: "",
-            alt: "Casamigos Tequila",
-            credit: "",
+            text: "Rum",
+            image: "https://images.unsplash.com/photo-1613140506142-277c6241b858?q=80&w=1528&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            alt: "A bottle of Kraken Rum",
+            credit: "Photo by Knighthawk Shoots on Unsplash",
         }
     ]
 
@@ -52,27 +52,27 @@ const questions = [
     answers: [
         {
             text: "Sour",
-            image: "",
+            image: "https://images.unsplash.com/photo-1568569350062-ebfa3cb195df?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
             alt: "Lemon",
-            credit: "",
+            credit: "Photo by Han Lahandoe on Unsplash",
         },
         {
             text: "Sweet",
-            image: "",
-            alt: "Sugar",
-            credit: "",
+            image: "https://images.unsplash.com/photo-1585155113372-6c1808141bf3?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            alt: "Sugar Cane",
+            credit: "Photo by Victoria Priessnitz on Unsplash",
         },
         {
             text: "Bitter",
-            image: "",
-            alt: "Coffee",
-            credit: "",
+            image: "https://images.unsplash.com/photo-1447933601403-0c6688de566e?q=80&w=1522&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            alt: "Coffee Beans",
+            credit: "Photo by Mike Kenneally on Unsplash",
         },
         {
             text: "Spicy",
-            image: "",
-            alt: "Chili Pepper",
-            credit: "",
+            image: "https://images.unsplash.com/photo-1546860255-95536c19724e?q=80&w=1608&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            alt: "Chili Peppers",
+            credit: "Photo by Shane Kong on Unsplash",
         }
     ]
 },
@@ -82,27 +82,27 @@ const questions = [
     answers: [
         {
             text: "It's just me",
-            image: "",
-            alt: "",
-            credit: "",
+            image: "https://images.unsplash.com/photo-1542029001094-3396a824ba27?q=80&w=1587&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            alt: "A single chair",
+            credit: "Photo by Markus Spiske on Unsplash",
         },
         {
             text: "Myself and a friend",
-            image: "",
-            alt: "",
-            credit: "",
+            image: "https://images.unsplash.com/photo-1581994574720-f60299904853?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            alt: "A table for two at a restaturant",
+            credit: "Photo by Evelyn Semenyuk on Unsplash",
         },
         {
             text: "A small gathering of friends",
-            image: "",
-            alt: "",
-            credit: "",
+            image: "https://images.unsplash.com/photo-1529903384028-929ae5dccdf1?q=80&w=1528&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            alt: "Four women sitting on a bench next to a dog",
+            credit: "Photo by Chris Murray on Unsplash",
         },
         {
             text: "A large group",
-            image: "",
-            alt: "",
-            credit: "",
+            image: "https://images.unsplash.com/photo-1581954548122-4dff8989c0f7?q=80&w=1530&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            alt: "Many people at a table for a dinner party",
+            credit: "Photo by Sebastian Coman Photography on Unsplash",
         }
     ]
 }
